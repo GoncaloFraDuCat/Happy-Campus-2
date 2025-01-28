3df375e760b0459426e122d742faf080cf375876
Goncalo Catarro <goncalocatarro@gmail.com>
2025-01-28 17:36:07 +0000

    organized

---
+++ js/navbar.js
@@ -0,0 +1,24 @@
+document.write(`
+  <nav>
+    <a href="/index.html">
+      <img src="/images/HC-logo-black.png" alt="Logo" sizes="64x64" class="navbar-logo">
+    </a>
+    <ul class="navbar">
+      <li><a href="/index.html#home">Home</a></li>
+      <li><a href="/index.html#sobre">Sobre</a></li>
+      <li><a href="/index.html#/atividades">Atividades</a></li>
+      <li><a href="/index.html#servicos">Serviços</a></li>
+      <li><a href="/index.html#noticias">Notícias</a></li>
+      <li><a href="#sobre">Recursos</a>
+        <ul class="dropdown-menu">
+          <li><a href="/recursos/webinars.html" class="nav-drop-link">Webinars</a></li>
+          <li><a href="/recursos/flyers.html" class="nav-drop-link">Flyers</a></li>
+          <li><a href="/recursos/news_&_media.html" class="nav-drop-link">News & Media</a></li>
+          <li><a href="/recursos/videos.html" class="nav-drop-link">Vídeos</a></li>
+        </ul>
+      </li>
+      <li><a href="/eventos.html">Eventos</a></li>
+    </ul>
+    <img src="/images/logos.png" alt="Logo" class="navbar-logo-1">
+  </nav>
+`);
